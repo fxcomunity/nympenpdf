f66aa454dc660fed075c4be2ebe6c8ebdec69918
fxcomunity <aqsholhalqi4@gmail.com>
2026-01-01 01:58:32 +0700

    Delete api/debug-env.js

--- api/debug-env.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const url = process.env.DATABASE_URL || "";
-  res.status(200).json({
-    hasDatabaseUrl: !!url,
-    startsWith: url.slice(0, 20),
-    containsBase: url.includes("base"),
-    length: url.length,
-  });
-}
