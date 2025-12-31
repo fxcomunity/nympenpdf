108c5e94d5636361824c0d152388b58fff1faea7
fxcomunity <aqsholhalqi4@gmail.com>
2026-01-01 03:58:48 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,26 +1,14 @@
-import { neon } from "@neondatabase/serverless";
-
 export default async function handler(req, res) {
-  try {
-    if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-
-    const { username, password } = req.body;
-
-    const sql = neon(process.env.DATABASE_URL);
-
-    const result = await sql`
-      SELECT * FROM admin_settings
-      WHERE username = ${username} AND password = ${password}
-      LIMIT 1
-    `;
-
-    if (result.length === 0) {
-      return res.status(401).json({ error: "Username / Password salah!" });
-    }
-
-    res.status(200).json({ success: true });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
   }
+
+  const { username, password } = req.body;
+
+  // contoh data hardcode (nanti kita pakai database neon admin_users)
+  if (username === "admin" && password === "admin123") {
+    return res.status(200).json({ success: true });
+  }
+
+  return res.status(401).json({ error: "Username atau password salah" });
 }
